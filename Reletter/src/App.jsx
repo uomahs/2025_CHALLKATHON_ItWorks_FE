bd2551f3a44b64695b1d1f5aa469f5411ff6807b
yewon <uomah.song@gmail.com>
2025-06-22 20:17:53 +0900

    로그인 추가 1

--- Reletter/src/App.jsx
+++ Reletter/src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "../src/pages/MainPage";
 import Login from "../src/pages/Login";
-import Home from "../src/pages/Home";
+import Start from "../src/pages/Start";
+import Signup from "../src/pages/Signup";
 
 import "./App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Start />} />
       <Route path="/main" element={<MainPage />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }

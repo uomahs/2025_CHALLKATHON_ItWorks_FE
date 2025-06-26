ffb94cd9f011b2fe13ff96dcc67ebee2b7bf71c1
nooyag8 <qkrrkdbs814@gmail.com>
2025-06-26 19:12:28 +0900

    front

--- Reletter/vite.config.js
+++ Reletter/vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: '${process.env.REACT_APP_API_URL}',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    ...(mode === 'development' && {
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:4000', // ✅ 실제 개발 서버 주소
+            changeOrigin: true,
+            rewrite: path => path.replace(/^\/api/, ''),
+          },
+        },
       },
-    },
-  },
+    }),
+  };
 });
